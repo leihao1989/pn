8469f505937088baade741c483ed2f9b112ebfdd
leihao1989 <leihao1127@sina.com>
2020-12-28 11:36:30 +0800

    接口测试框架源码

---
+++ tests/test_001.py
@@ -0,0 +1,18 @@
+#！/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+#Author：雷浩
+
+import pytest
+
+def test_day1():
+	a=1
+	b=2
+	c=a+b
+	assert c==3
+
+def test_day2():
+	a=1
+	b=4
+	c=a+b
+	assert c==5
